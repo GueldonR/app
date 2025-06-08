260ce5ba833ef3b20c01e0ab055ae8458d179c27
gueldon <149371777+GueldonR@users.noreply.github.com>
2025-06-08 21:28:34 +0200

    Refactor article repository implementation: complete getNewsArticles method and enhance error handling

--- lib/Features/dailynews/Data/repository/article_repostitory_impl.dart
+++ lib/Features/dailynews/Data/repository/article_repostitory_impl.dart
@@ -1,12 +1,42 @@
 // gör om hela filen
+import 'dart:io';
+
+import 'package:app/Features/dailynews/Data/data_sources/remote/news_api_service.dart';
 import 'package:app/Features/dailynews/Domain/entities/article_entity.dart';
 import 'package:app/Features/dailynews/Domain/repositories/article_repository.dart';
+import 'package:app/shared/core/constants/constants.dart';
 import 'package:app/shared/core/resources/data_state.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
 
 class ArticleRepostitoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepostitoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleEntity>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleEntity>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: apiKeyNews,
+        category: queryCategory,
+        country: queryCountry,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccessState(httpResponse.data);
+      } else {
+        return DataErrorState(
+          DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioException catch (error) {
+      return DataErrorState(error);
+    }
   }
 }
--- lib/shared/core/constants/constants.dart
+++ lib/shared/core/constants/constants.dart
@@ -1,5 +1,9 @@
+import 'package:retrofit/retrofit.dart';
+
 const String newsBaseUrl = 'https://newsapi.org/v2/';
 
+const String apiKeyNews = '753608f5ac0448b0bb661f7ddee4b1ab';
 
+const String queryCountry = 'sv';
 
-// lägg till rätt api
+const String queryCategory = 'general';
